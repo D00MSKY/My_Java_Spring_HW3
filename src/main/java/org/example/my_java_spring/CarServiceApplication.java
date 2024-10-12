763c32721ad2933376c7f30b78dc9a86715619ce
D00MSKY <vetal445@gmail.com>
2024-10-12 23:07:23 +0300

    HW 1 version 2 complete

--- src/main/java/org/example/my_java_spring/CarServiceApplication.java
+++ src/main/java/org/example/my_java_spring/CarServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MyJavaSpringApplication {
+public class CarServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MyJavaSpringApplication.class, args);
+        SpringApplication.run(CarServiceApplication.class, args);
     }
 
 }
--- src/main/java/org/example/my_java_spring/properties/ReferenceDataProperties.java
+++ src/main/java/org/example/my_java_spring/properties/ReferenceDataProperties.java
@@ -1,6 +1,7 @@
 package org.example.my_java_spring.properties;
 
 import lombok.Data;
+import lombok.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
